4dd2c6904754dff293b5ecd53d8151176b94d1f6
Ensar007 <ensar.sehovic@elev.ga.ntig.se>
2026-05-13 13:44:35 +0200

    ändrade lite backenden

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,43 @@
 import express from "express";
 import {Client} from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend fungerar!");
   });
 
+  app.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+  
+    try {
+      const result = await db.query(
+        "INSERT INTO accounts (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+        [username, email, password]
+      );
+  
+      res.status(201).json({
+        message: "Användare skapad",
+        user: result.rows[0],
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Något gick fel",
+      });
+    }
+  });
+
   const db = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "MatLeverans",
-    password: 'Bugatti88.',
-    port: 3000,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
   });
   db.connect();
 
---
+++ frontend/signup.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("signupForm");
+  
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+  
+      console.log("Submit funkar");
+  
+      const firstName = document.getElementById("firstName").value;
+      const lastName = document.getElementById("lastName").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+  
+      const username = firstName + " " + lastName;
+  
+      try {
+        const response = await fetch("http://localhost:3010/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+          }),
+        });
+  
+        const data = await response.json();
+        console.log(data);
+  
+        alert("Konto skapat!");
+      } catch (error) {
+        console.error(error);
+        alert("Något gick fel");
+      }
+    });
+  });
